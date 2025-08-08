7db7923da71a1a474dfe5cbec7b8de7a33c35bf0
kainat224 <anmolkainat244@gmail.com>
2025-08-09 04:16:12 +0500

    work on create and browse jobs page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import { useSession } from 'next-auth/react';
+import { logout } from '@/lib/auth';
 
 const Navbar = () => {
+  const {data: session} = useSession()
   return (
     <nav className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -29,7 +33,7 @@ const Navbar = () => {
             >
               Browse Jobs
             </Link>
-            {false ? (
+            {session ? (
               <>
                 <Link
                   href="/jobs/post"
@@ -43,9 +47,7 @@ const Navbar = () => {
                 >
                   Dashboard
                 </Link>
-                <button
-                  className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
-                >
+                <button onClick={logout} className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">
                   Sign Out
                 </button>
               </>
